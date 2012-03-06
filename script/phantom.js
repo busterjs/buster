aa4109311f3cee5a3fadd6ddc9a0afa47cdffb1d
Tiago Rodrigues <tiago@top10.com>
2012-03-06 15:03:30 +0000

    adding console event handler on phantom script to redirect console output to terminal

--- script/phantom.js
+++ script/phantom.js
@@ -2,6 +2,9 @@ if(!phantom.state) {
     phantom.state = "buster";
     var page = new WebPage();
     page.open("http://localhost:1111/capture", function(status) {
-          console.log(status);
-        });
+      console.log(status);
+      page.onConsoleMessage = function () {
+        console.log.apply(console, arguments);
+      };
+    });
 }
