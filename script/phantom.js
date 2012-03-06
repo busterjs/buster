f72ee24b6016b4f6d1d24fe8692da4821482c155
Tiago Rodrigues <tiago@top10.com>
2012-03-06 15:23:34 +0000

    improving console event handler on phantom script to provide more useful output with line number and file name

--- script/phantom.js
+++ script/phantom.js
@@ -1,10 +1,23 @@
+phantom.silent = false;
+
 if(!phantom.state) {
     phantom.state = "buster";
     var page = new WebPage();
+
     page.open("http://localhost:1111/capture", function(status) {
-      console.log(status);
-      page.onConsoleMessage = function () {
-        console.log.apply(console, arguments);
-      };
+      if(!phantom.silent){
+        console.log(status);
+
+        page.onConsoleMessage = function (msg, line, id) {
+          var fileName = id.split('/');
+          // format the output message with filename, line number and message
+          // weird gotcha: phantom only uses the first console.log argument it gets :(
+          console.log(fileName[fileName.length-1]+', '+ line +': '+ msg);
+        };
+
+        page.onAlert = function(msg) {
+          console.log(msg);
+        };
+      }
     });
 }
